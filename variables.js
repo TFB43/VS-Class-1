46ce90f90cff097bda42b333fe90b31ed32eede9
TFB43 <99685292+TFB43@users.noreply.github.com>
2022-03-03 13:58:20 -0600

    javaScript essentials

---
+++ variables.js
@@ -0,0 +1,11 @@
+var fullName = "John Doe";
+
+var fistName = "John Doe";
+console.log(fistName);
+
+firstName = "Skylar Mendoza";
+let age =10;
+age = 50;
+const dogNames = ["charlie", "Brownie", "pinkie"];
+dogNames[0] = "apollo";
+console.log(dogNames);
